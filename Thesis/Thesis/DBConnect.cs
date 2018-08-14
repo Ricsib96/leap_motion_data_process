5938f47aa2330b1608b3c7e621e58b0ce3e44e93
Ricsi <ricsib96@gmail.com>
2018-08-14 21:33:11 +0200

    delete, add functions

--- Thesis/Thesis/DBConnect.cs
+++ Thesis/Thesis/DBConnect.cs
@@ -14,10 +14,21 @@ namespace Thesis
     class DBConnect
     {
         private MySqlConnection connection;
-        private string server;
-        private string database;
-        private string uid;
-        private string password;
+
+        private const string SERVER = "localhost";
+        private const string DATABASE = "data_process";
+        private const string UID = "root";
+        private const string PASSWORD = "admin";
+
+        private const string TABLE_PATIENTS = "patients";
+
+        private const string COL_ID = "id";
+        private const string COL_FIRST_NAME = "first_name";
+        private const string COL_LAST_NAME = "last_name";
+        private const string COL_ADDRESS = "address";
+        private const string COL_BIRTH = "birth";
+        private const string COL_SEX = "sex";
+        private const string COL_TEL_NUMBER = "tel_number";
 
         //Constructor
         public DBConnect()
@@ -28,13 +39,10 @@ namespace Thesis
         //Initialize values
         private void Initialize()
         {
-            server = "localhost";
-            database = "data_process";
-            uid = "root";
-            password = "admin";
+
             string connectionString;
-            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
-            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connectionString = "SERVER=" + SERVER + ";" + "DATABASE=" +
+            DATABASE + ";" + "UID=" + UID + ";" + "PASSWORD=" + PASSWORD + ";";
 
             connection = new MySqlConnection(connectionString);
         }
@@ -95,16 +103,39 @@ namespace Thesis
         public void Update()
         {
         }
+        public void addPatient(NameValueCollection newData)
+        {
+            string query =  "INSERT INTO "+ TABLE_PATIENTS + "(" + 
+                            COL_FIRST_NAME + "," + COL_LAST_NAME + "," +
+                            COL_ADDRESS + "," + COL_BIRTH + "," +
+                            COL_SEX + "," + COL_TEL_NUMBER + ")" + 
+                            
+                            "VALUES (" + "'" + newData[COL_FIRST_NAME] + "'" + "," + 
+                                        "'" + newData[COL_LAST_NAME] + "'" + "," +
+                                        "'" + newData[COL_ADDRESS] + "'" + "," +
+                                        "'" + newData[COL_BIRTH] + "'" + "," +
+                                        "'" + newData[COL_SEX] + "'" + "," +
+                                        "'" + newData[COL_TEL_NUMBER] + "'" + ")";
 
+            OpenConnection();
+            if (this.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                Trace.Write(query);
+                CloseConnection();
+            }
+
+        }
         public void updatePatient(NameValueCollection newData)
         {
-            string query = "UPDATE patients " +
-                            "SET    first_name = '" + newData["firstname"] + "', " +
-                                    "last_name = '" + newData["lastname"] + "', " +
-                                    "address = '" + newData["address"] + "', " +
-                                    "birth = '" + newData["birth"] + "', " +
-                                    "tel_number = '" + newData["tel"] + "' " +
-                            "WHERE  id = '" + newData["id"] + "'";
+            string query = "UPDATE "+ TABLE_PATIENTS +
+                            " SET    " + COL_FIRST_NAME + " = '" + newData[COL_FIRST_NAME] + "', " +
+                                    COL_LAST_NAME +" = '" + newData[COL_LAST_NAME] + "', " +
+                                    COL_ADDRESS +" = '" + newData[COL_ADDRESS] + "', " +
+                                    COL_BIRTH +" = '" + newData[COL_BIRTH] + "', " +
+                                    COL_TEL_NUMBER +" = '" + newData[COL_TEL_NUMBER] + "' " +
+                            "WHERE  " + COL_ID +" = '" + newData[COL_ID] + "'";
 
             OpenConnection();
             if (this.OpenConnection())
@@ -136,13 +167,10 @@ namespace Thesis
                 {
                     NameValueCollection temp = new NameValueCollection();
 
-                    temp.Add("firstname", reader.GetString("first_name"));
-                    temp.Add("lastname", reader.GetString("last_name"));
-                /*    temp.Add("address", reader.GetString("address"));
-                    temp.Add("birth", reader.GetString("birth"));
-                    temp.Add("tel", reader.GetString("tel_number"));
-                    temp.Add("sex", reader.GetString("sex"));
-                */
+                    temp.Add("firstname", reader.GetString(COL_FIRST_NAME));
+                    temp.Add("lastname", reader.GetString(COL_LAST_NAME));
+                    temp.Add("id", reader.GetString(COL_ID));
+                    
                     list.Add(temp);
                 }
                 CloseConnection();
@@ -153,12 +181,12 @@ namespace Thesis
                 return list;
             }
         }
-        public List<NameValueCollection> getPatientByName(String firstname, String lastname)
+        public NameValueCollection getPatientByName(String firstname, String lastname)
         {
-            string query = "SELECT * FROM patients " +
-                            "WHERE first_name = '" + firstname + 
-                            "' AND last_name = '" + lastname + "'";
-            List<NameValueCollection> list = new List<NameValueCollection>();
+            string query = "SELECT * FROM " + TABLE_PATIENTS + " " +
+                            "WHERE " + COL_FIRST_NAME +" = '" + firstname + 
+                            "' AND " + COL_LAST_NAME +" = '" + lastname + "'";
+            NameValueCollection temp = new NameValueCollection();
 
             if (this.OpenConnection())
             {
@@ -167,22 +195,61 @@ namespace Thesis
 
                 while (reader.Read())
                 {
-                    NameValueCollection temp = new NameValueCollection();
 
-                    temp.Add("id", reader.GetString("id"));
-                    temp.Add("firstname", reader.GetString("first_name"));
-                    temp.Add("lastname", reader.GetString("last_name"));
-                    temp.Add("address", reader.GetString("address"));
-                    temp.Add("birth", reader.GetString("birth"));
-                    temp.Add("tel", reader.GetString("tel_number"));
-                    
-                    list.Add(temp);
+                    temp.Add("id", reader.GetString(COL_ID));
+                    temp.Add("firstname", reader.GetString(COL_FIRST_NAME));
+                    temp.Add("lastname", reader.GetString(COL_LAST_NAME));
+                    temp.Add("address", reader.GetString(COL_ADDRESS));
+                    temp.Add("birth", reader.GetString(COL_BIRTH));
+                    temp.Add("tel", reader.GetString(COL_TEL_NUMBER));
+                   
                 }
                 CloseConnection();
             }
-                return list;
+            return temp;
         }
 
+        public NameValueCollection getPatientById(int id)
+        {
+            string query = "SELECT * FROM " + TABLE_PATIENTS + " " +
+                            "WHERE " + COL_ID + " = '" + id.ToString() + "'";
+            NameValueCollection temp = new NameValueCollection();
+
+            if (this.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    temp.Add("id", reader.GetString(COL_ID));
+                    temp.Add("firstname", reader.GetString(COL_FIRST_NAME));
+                    temp.Add("lastname", reader.GetString(COL_LAST_NAME));
+                    temp.Add("address", reader.GetString(COL_ADDRESS));
+                    temp.Add("birth", reader.GetString(COL_BIRTH));
+                    temp.Add("tel", reader.GetString(COL_TEL_NUMBER));
+
+                }
+                CloseConnection();
+            }
+            return temp;
+        }
+        public void deletePatientById(int id)
+        {
+            string query = "DELETE FROM " + TABLE_PATIENTS +
+                            " WHERE " + COL_ID + " = '" + id + "'";
+
+            OpenConnection();
+            if (this.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                Trace.Write(query);
+                CloseConnection();
+            }
+
+        }
 
         //Count statement
         /*    public int Count()
